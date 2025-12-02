d972b65dc8e89c4de37690d165646374c1c1c8da
Alebyr124 <alexisbyrne121@gmail.com>
2025-12-02 00:38:45 -0300

    Cambio para codecov

--- TarjetaSubeTest/obj/Debug/net5.0/TarjetaSubeTest.AssemblyInfo.cs
+++ TarjetaSubeTest/obj/Debug/net5.0/TarjetaSubeTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarjetaSubeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51afccbb69525b2ca4559b68c3013b99fa9aa162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334c1c5004fa395c6e7f48431dc9e6ffa8de3210")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarjetaSubeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarjetaSubeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
